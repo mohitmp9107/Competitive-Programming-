31277bdcf1ba0d944bf5927fa5f0ce7d54c6ea88
MOHIT SAINI <43476438+mohitmp9107@users.noreply.github.com>
2020-02-23 00:56:19 +0530

    Prime Factors of a No in O( sqrt(N) )

--- Prime_Factorization.cpp
+++ Prime_Factorization.cpp
@@ -1,23 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int primefactorization(int a ){
+typedef long long ll;
+#define pb push_back
+vector<long long> v;
+ll primefactorization(ll a ){
 
     while(a%2==0){
         cout << 2 << endl;
         a/=2;
     }
-    
-    for(int i=3;i<=sqrt(a);i+=2){
+    // cout << a << endl;
+    for(ll i=3;i<=sqrt(a);i+=2){
         while(a%i==0){
             cout << i << endl;
             a/=i;
         }
 
     }
-    if(a>2) cout << a <<endl;
+    if(a>2)
+        cout << a <<endl;
 }
 int main()
 {
-    int a;   cin >> a;
-    primefactorization(a) ;
+    ll a;   cin >> a;
+    cout << primefactorization(a) << endl ;
 }
