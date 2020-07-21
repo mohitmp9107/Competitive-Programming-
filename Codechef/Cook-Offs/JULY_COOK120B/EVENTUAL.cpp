8f55e36a076239fffa313f8c92c8af2fe5fea6f2
Mohit Saini <mohitsaininith@gmail.com>
2020-07-21 08:46:39 +0530

    Added July cookOff

---
+++ Codechef/Cook-Offs/JULY_COOK120B/EVENTUAL.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+#define rep(i, n)    for(ll i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(ll i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(ll i = a; i >= (n); --i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (ll)(x).size()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fill(a)  memset(a, 0, sizeof (a))
+#define ff first
+#define ss second
+#define pb push_back
+typedef long double ld;
+typedef vector<ll> vll;
+typedef vector<pair<ll,ll>> vpl;
+const ll INF = 1e18;
+const ld PI = 4*atan((ld)1);
+const ll mod = 1e9+7;
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    ll tt=1;
+        cin >> tt;
+    while((tt)--){
+        ll n ; cin >> n;
+        string s; cin >> s;
+        map<ll,ll> m;
+        rep(i,n){
+            m[s[i]]++;
+        }
+        ll f=0;
+        trav(i,m){
+            if(i.ss%2){
+                f=1; break;
+            }
+        }
+        if(f){
+            cout << "NO" << endl;
+        }else{
+            cout << "YES" << endl;
+        }
+    }
+}
